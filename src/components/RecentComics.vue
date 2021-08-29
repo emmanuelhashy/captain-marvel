f5e37d2d5c2aa3d6f71cca2d830e867a38b7ac73
Emmanuelhashy <Emmanuelhashy@gmail.com>
2021-08-29 05:06:01 +0100

    remove splice method

--- src/components/RecentComics.vue
+++ src/components/RecentComics.vue
@@ -72,7 +72,7 @@ export default {
       let res = await this.$http.get(
         `${baseURL}comics?apikey=d2a508ec092852bfb6b4d607085c6e08`
       );
-      this.comics = res.data.results.splice(0,6)
+      this.comics = res.data.results
       console.log("commics", this.comics)
     },
   },
